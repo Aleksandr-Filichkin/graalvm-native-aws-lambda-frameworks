02e311128213475a8ed3a568ceb7b98eca52cd4f
Aleksandr_Filichkin <Aleksandr.Filichkin@refinitiv.com>
2022-04-05 22:44:25 +0200

    support java 11 and java 17

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
-mkdir -p "output"
+mkdir -p "graalvm_latest"
+mkdir -p "graalvm_11-21.3"
 
 #builds a native binary and zip
 mvn clean install -P native
@@ -11,7 +12,14 @@ docker build -f Dockerfile.graalvm -t lambda-graalvm .
 #
 #copy from the docker container to host
 containerId=$(docker create -ti lambda-graalvm bash)
-docker cp ${containerId}:/tmp/dist output
+docker cp ${containerId}:/tmp/dist graalvm_latest
+
+#builds a native binary and zip
+docker build -f Dockerfile.graalvm-java11-21.3 -t lambda-graalvm-11-21.3 .
+#
+#copy from the docker container to host
+containerId=$(docker create -ti lambda-graalvm-11-21.3)
+docker cp ${containerId}:/tmp/dist graalvm_11-21.3
 
 ## Deploy lambdas
 
